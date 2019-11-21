6643553fc7eca0ddbbee33b4194fc9183fda5908
JuanGarciaReyes <45839913+JuanGarciaReyes@users.noreply.github.com>
2019-11-20 19:32:55 -0700

    Add files via upload

--- lab5.py
+++ lab5.py
@@ -6,111 +6,90 @@ Created on Wed Nov 20 16:48:06 2019
 """
 
 class Node:
-    def __init__(self, item, next = None, prev = None, key = None):
-        self.item = item;
+    def __init__(self, key, value, next = None, prev = None):
+        self.item = value;
         self.next = next;
         self.prev = prev
         self.key = key
 
+class LRUcache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.dict = {}
+        self.head = None
+        self.tail = None
 
-class DoublyLinkedList:
-    def __init__(self, head = None, tail = None):
-        self.head = head
-        self.tail = tail
-
-
-    def insertAtStart(self, item):
-        new_node = Node(item)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.head.prev = new_node
-            new_node.next = self.head
-            self.head = new_node
-
-    def insertAtEnd(self, item):
-        new_node = Node(item)
-        if self.head is None:
-            self.head = new_node
-            self.tail = self.head
-            
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = self.tail.next
-            self.tail.next = None
-
-
-    def remove(self, value):
-        
-        if self.head.item == value:
-            self.head = self.head.next
-            self.head.prev = None
-            return self.head
-        elif self.tail.item == value:
+    def remove(self, node):
+        if self.head is None and self.tail is None:
+            return None
+        elif self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.head:
+          self.head = self.head.next  
+        elif node == self.tail:
             self.tail = self.tail.prev
-            self.tail.next = None
-            return self.tail
-        else:
-            temp = self.head.next
-            while temp.next is not None:
-                if temp.item == value:
-                    temp.prev.next = temp.next
-                    temp.next.prev = temp.prev
-                    return temp
-                else:
-                    temp = temp.next
-    def printDouble(self):
+        else: #middle
+            previous_node = node.prev
+            next_node = node.next
+            previous_node.next = next_node
+            next_node.prev = previous_node
+    
+    def add (self, node):
+        if self.head is None and self.tail is None:
+            self.head = node
+            self.tail = node
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
+        self.dict[node.key] = node
+        
+        
+    def printing(self):
         temp = self.head
         while temp is not None:
             print(temp.item, end=' ')
             temp = temp.next
-            
-class LRUcache:
-    def __init__(self, capacity):
-        self.capacity =capacity
-        self.dict = {}
-        self.size = 0
+        print()
 
-    def set_head(self, node):
-        if not self.head:
-            self.head = node
-            self.tail = node
-        else:
-            node.prev = self.head
-            self.head.next = node
-            self.head = node
-        self.current_size += 1
-    
     def get(self, key):
         if key not in self.dict:
             return -1
         else:
-            return self.dict[key]
-    
+            node = self.dict[key]
+            self.remove(node)
+            self.add(node)
+            return node.item
+
     def put(self, key, value):
         if key in self.dict:
-            node = self.dict[key]
-            node.item = value
-            
-        else:
-            new_node = Node(key, value)
-            if self.size == self.capacity:
-                del self.dict[self.tail.key]
-                self.remove(self.tail)
-            self.set_head(new_node)
-            self.hash_map[key] = new_node
+            self.remove(self.dict[key])
+        new_node = Node(key, value)
+        self.add(new_node)
+        self.dict[key] = new_node
+        if len(self.dict) > self.capacity:
+            del self.dict[self.head.key]
+            self.head = self.head.next
+       
+        
         
 
 if __name__ == '__main__':
-    dll = DoublyLinkedList()
-    dll.insertAtStart(1)
-    dll.insertAtStart(2)
-    dll.insertAtEnd(3)
-    dll.insertAtStart(4)
-    dll.printDouble()
-    dll.remove(2)
-    print()
-    dll.printDouble()
+    
+    cache = LRUcache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    cache.printing()
+    print(cache.get(1))       #// returns 1
+    cache.put(3, 3)
+    cache.printing()    #// evicts key 2
+    print(cache.get(2))      #// returns -1 (not found)
+    cache.put(4, 4)
+    #// evicts key 1
+    cache.printing()
+    print(cache.get(1))       #// returns -1 (not found)
+    print(cache.get(3))       #// returns 3
+    print(cache.get(4)) #evicts key 1
+
+    
  
